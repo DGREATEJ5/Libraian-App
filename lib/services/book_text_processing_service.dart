2dfdf2088f3e2775649bb537c63779c45ef48c2d
DGREATEJ5 <96094710+DGREATEJ5@users.noreply.github.com>
2024-11-03 14:53:34 +0800

    Update book_text_processing_service.dart

--- lib/services/book_text_processing_service.dart
+++ lib/services/book_text_processing_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 Future<Map<String, dynamic>> processBookText(String text) async {
-  final url = Uri.parse('https://final-book-model-backend-1b41e7bf7fbc.herokuapp.com/predict');
+  final url = Uri.parse('YOUR-SECRET-API');
   final response = await http.post(
     url,
     headers: {'Content-Type': 'application/json'},
