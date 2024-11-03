d73cc78354510acbb1c6bdf042016e351d1b1a35
DGREATEJ5 <96094710+DGREATEJ5@users.noreply.github.com>
2024-11-03 14:53:04 +0800

    Update text_processing_service.dart

--- lib/services/text_processing_service.dart
+++ lib/services/text_processing_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 Future<Map<String, dynamic>> processText(String text) async {
-  final url = Uri.parse('https://final-thesis-model-a5eb5b7e0b84.herokuapp.com/predict');
+  final url = Uri.parse('YOUR-SECRET-API');
   final response = await http.post(
     url,
     headers: {'Content-Type': 'application/json'},
